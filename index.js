453211c6d921ccf99d98dde6f2c1840f177d3ebf
chrisdcalver <chrisdcalver@gmail.com>
2021-07-10 16:39:38 +0100

    Building out functions for main game

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ let blackjack = true
 let message = ""
 
 // DOM
-/*
-let commentEl = document.getElementById("comment-el")
+
+// let commentEl = document.getElementById("comment-el")
 let cardsEl = document.getElementById("cards-el")
-let sumEl = document.getElementById("sum-el")
-*/
+// let sumEl = document.getElementById("sum-el")
+
 // Function to generate random card. This will be used by other functions to generate cards.
 
 function randomCard() {
     newCard = Math.floor(Math.random() * 13) + 1
+
     if (newCard === 1) {
         return 11
     } else if (newCard > 10) {
@@ -25,3 +26,22 @@ function randomCard() {
     }
 }
 
+// Start game button: generates 2 x new random cards and adds them to cards[] and var sum. 
+
+function startGame() {
+    isAlive = true
+    let firstCard = randomCard()
+    let secondCard = randomCard()
+    cards.push(firstCard, secondCard)
+    sum = firstCard + secondCard
+}
+
+// Main game play that other functions will use to play. Also controls the message seen on screen.
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+
+    for (i = 0; i < cards.length; i++) {
+
+    }
+}
